2c535c1f6e88c0b1c27729480ab5f5392fc36790
AlekseyScull <ficusoff@gmail.com>
2023-03-04 15:06:37 +0500

    homework 1

---
+++ src/homework1/Guess.java
@@ -0,0 +1,29 @@
+package homework1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Guess {
+    public static void main(String[] args) throws Exception {
+        int number = new Random().nextInt(99) + 1;
+        int maxAttempts = 10;
+        System.out.println("Я загадал число. У тебя " + maxAttempts + " попыток угадать.");
+        try (Scanner scanner = new Scanner(System.in)) {
+            for (int i = maxAttempts - 1; i >= 0; i--) {
+                int guess = scanner.nextInt();
+                if (guess == number) {
+                    System.out.println("Ты угадал с " + (maxAttempts - i) + " попытки!");
+                    break;
+                }
+                if (i > 0) {
+                    if (guess < number) {
+                        System.out.print("Мое число больше! ");
+                    } else {
+                        System.out.print("Мое число меньше! ");
+                    }
+                    System.out.println("У тебя осталось " + (i) + " попыток");
+                } else System.out.println("Ты не угадал");
+            }
+        }
+    }
+}
