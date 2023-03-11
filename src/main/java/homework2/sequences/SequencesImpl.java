3c53ec0798f955541819aa3f4b54f26e39987681
AlekseyScull <ficusoff@gmail.com>
2023-03-11 19:01:03 +0500

    Homework 2, Sequences

---
+++ src/main/java/homework2/sequences/SequencesImpl.java
@@ -0,0 +1,99 @@
+package homework2.sequences;
+
+public class SequencesImpl implements Sequences {
+    @Override
+    public void a(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i * 2 + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void b(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print((i * 2 - 1) + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void c(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print((i * i) + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void d(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print((i * i * i) + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void e(int n) {
+        int result;
+        for (int i = 1; i <= n; i++) {
+            result = i % 2 == 0 ? -1 : 1;
+            System.out.print(result + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void f(int n) {
+        int result;
+        for (int i = 1; i <= n; i++) {
+            result = (i % 2 == 0 ? -1 : 1) * i;
+            System.out.print(result + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void g(int n) {
+        int result;
+        for (int i = 1; i <= n; i++) {
+            result = (i % 2 == 0 ? -1 : 1) * i * i;
+            System.out.print(result + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void h(int n) {
+        int counter = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) System.out.print(counter++ + " ");
+            else System.out.print(0 + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void i(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+            System.out.print(result + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void j(int n) {
+        long[] fib = new long[n + 1];
+        fib[1] = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i < 2) System.out.print(fib[i] + " ");
+            else {
+                fib[i] = fib[i - 1] + fib[i - 2];
+                System.out.print(fib[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+}
---
+++ src/main/java/homework2/sequences/SequencesTest.java
@@ -0,0 +1,17 @@
+package homework2.sequences;
+
+public class SequencesTest {
+    public static void main(String[] args) {
+        SequencesImpl sequences = new SequencesImpl();
+        sequences.a(20);
+        sequences.b(20);
+        sequences.c(20);
+        sequences.d(20);
+        sequences.e(20);
+        sequences.f(20);
+        sequences.g(20);
+        sequences.h(20);
+        sequences.i(20);
+        sequences.j(20);
+    }
+}
