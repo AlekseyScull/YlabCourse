c42dab451f63873d6a54a442bd7fc8f18b1e4c0c
AlekseyScull <ficusoff@gmail.com>
2023-03-16 11:41:55 +0500

    Simplify validator

--- src/main/java/homework2/snilsvalidator/SnilsValidatorImpl.java
+++ src/main/java/homework2/snilsvalidator/SnilsValidatorImpl.java
@@ -4,22 +4,19 @@ public class SnilsValidatorImpl implements SnilsValidator {
     @Override
     public boolean validate(String snils) {
         boolean result = false;
-        if (snils.length() >= 9 &&
-                snils.length() <= 14 &&
-                snils.matches("[0-9 -]+")) {
-            String formattedString = snils.replaceAll("[- ]", "");
+        String formattedString = snils.replaceAll("[- ]", "");
+        if (formattedString.length() == 11 &&
+                formattedString.matches("\\d{11}")) {
             int checkDigit = Integer.parseInt(formattedString.substring(9));
             char[] arr = formattedString.toCharArray();
-            if (arr.length == 11) {
-                int sum = 0;
-                for (int i = 0; i < 9; i++) {
-                    sum += Character.digit(arr[i], 10) * (9 - i);
-                }
-                if ((sum < 100 && sum == checkDigit) ||
-                        (sum > 101 && checkDigit == sum % 101) ||
-                        (sum % 101 == 100 && checkDigit == 0)) {
-                    result = true;
-                }
+            int sum = 0;
+            for (int i = 0; i < 9; i++) {
+                sum += Character.digit(arr[i], 10) * (9 - i);
+            }
+            if ((sum < 100 && sum == checkDigit) ||
+                    (sum > 101 && checkDigit == sum % 101) ||
+                    (sum % 101 == 100 && checkDigit == 0)) {
+                result = true;
             }
         }
         return result;
